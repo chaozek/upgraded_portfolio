5592a33ed069b632cd7f57f34bb0971a9801b0f6
Pavel Kaplan <pavel.kaplan@siteone.cz>
2023-02-08 19:59:08 +0100

    test deploy

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,7 +12,7 @@ export const Skills = () => (
         fontSize="1xl"
         mb="5"
       >
-        Main focus (making a living from) TEST
+        Main focus (making a living from) TESTTESTTESTTEST
       </Text>
       <ItemList items={profiSkills} />
 
