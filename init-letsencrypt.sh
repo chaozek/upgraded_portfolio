fe343dc577e4f71fde7afd64fa70f2e5aef30063
pavel <pkaplan1@seznam.cz>
2023-02-12 11:48:24 +0000

    add www cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(brnensky-vyvojar.eu)
+domains=(www.brnensky-vyvojar.eu brnensky-vyvojar.eu)
 rsa_key_size=4096
 data_path="./certbot"
 email="chaozek@seznam.cz" # Adding a valid address is strongly recommended
